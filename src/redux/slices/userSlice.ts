81cc264afc19c5d62a6d9f280c2de49ee5deca28
Nikita <powerboy20042006@gmail.com>
2023-03-26 18:19:28 +0300

    name-fix

--- src/redux/slices/userSlice.ts
+++ src/redux/slices/userSlice.ts
@@ -1,14 +1,14 @@
 import { createSlice } from "@reduxjs/toolkit";
 interface User {
   mail: null | string,
-  name: null | string,
+  name: string,
   id: null | number,
   gender: null | number,
   age: null | number,
 }
 const initialState: User = {
   mail: null,
-  name: null,
+  name: '',
   id: null,
   gender: null,
   age: null,
@@ -22,7 +22,7 @@ const userSlice = createSlice({
     },
     removeUser(state) {
       state.mail = null;
-      state.name = null;
+      state.name = '';
       state.id = null;
     },
     setName(state, action) {
